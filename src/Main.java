2665c387c160842c5d5f9cb40f7edd5f0d90da60
methas-ru <methas_ru@cmu.ac.th>
2025-08-13 22:45:45 +0700

    try catch

--- src/Main.java
+++ src/Main.java
@@ -10,21 +10,36 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         StudentGrades studentGrades = new StudentGrades();
 
-        //input
-        System.out.println("Enter a series of scores (comma-separated):");
-        String score_String = scanner.nextLine();
+        try {
+            //input
+            System.out.println("Enter a series of scores (comma-separated):");
+            String score_String = scanner.nextLine();
 
-        //process
+            //process
 
-        //split
-        String[] score_Arr_String = score_String.split(",");
-        //parseInt
-        int[] score_Arr_Int = new int[score_Arr_String.length];
-        for (int i = 0; i < score_Arr_String.length; i++) {
-            score_Arr_Int[i] = Integer.parseInt(score_Arr_String[i]);
+            //split
+            String[] score_Arr_String = score_String.split(",");
+            //parseInt
+            int[] score_Arr_Int = new int[score_Arr_String.length];
+            for (int i = 0; i < score_Arr_String.length; i++) {
+                score_Arr_Int[i] = Integer.parseInt(score_Arr_String[i]);
+            }
+
+            double studentGrade_Avg = studentGrades.calculateAverage(score_Arr_Int);
+            System.out.println(studentGrade_Avg);
+        }
+        catch ( NumberFormatException e ) {
+            System.out.println("The user enters non-numeric input." +
+                    "\nErrorMessage: " + e.getMessage());
+        }
+        catch ( IllegalArgumentException e ) {
+            System.out.println("ErrorMessage: " + e.getMessage());
+        }
+        finally {
+            scanner.close();
+            System.out.println( "Grade calculation process concluded.");
         }
 
-        double studentGrade_Avg = studentGrades.calculateAverage(score_Arr_Int);
-        System.out.println(studentGrade_Avg);
     }
+
 }
